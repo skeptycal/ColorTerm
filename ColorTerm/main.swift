c42ee980fadea6bdba97f4b2d0980b5ca9202024
skeptycal <skeptycal@gmail.com>
2019-02-28 08:51:20 -0600

    Initial Commit

---
+++ ColorTerm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ColorTerm
+//
+//  Created by Michael Treanor on 2/28/19.
+//  Copyright © 2019 Michael Treanor. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
